45ab5cab101eef1d4c4207be48000217b03b0175
user <user@user-PC-43FB>
2015-10-27 21:51:00 +0800

    commit

--- src/module/Main.java
+++ src/module/Main.java
@@ -1,8 +1,12 @@
 package module;
 
+import dataPreprocessing.SAXTransformation;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");	
-    
+        SAXTransformation sax = new SAXTransformation();
+       
+        sax.start(args[0]);
     }
 }
